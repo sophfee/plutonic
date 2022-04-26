c3b8d2b1a78eb4adbe5bdc84f54d2877439c8396
Nick <66189083+urnotnick@users.noreply.github.com>
2022-04-26 13:10:15 -0500

    Create init.lua

---
+++ lua/weapons/ls_base/init.lua
@@ -0,0 +1,8 @@
+--      Copyright (c) 2022, Nick S. All rights reserved      --
+-- Longsword2 is a project built upon Longsword Weapon Base. --
+
+-- [ File Details ]
+-- Purpose: Loads all files to their correct clients.
+-- Todo: N/A
+
+include( "/client/viewmodel.lua" )
